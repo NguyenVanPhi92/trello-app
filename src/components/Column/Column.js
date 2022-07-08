4333d2df51b77d242a5490ed16efdfca5d1419f9
Phinv <nguyenvanphidmx123455@gmail.com>
2022-07-08 16:00:43 +0700

    Drap and Drop

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,5 +1,6 @@
 import Card from "components/Card/Card";
 import React from "react";
+import { Container, Draggable } from "react-smooth-dnd";
 import { mapOrder } from "utilities/sorts";
 import "./column.scss";
 
@@ -7,14 +8,35 @@ const Column = (props) => {
   const { column } = props;
   const cards = mapOrder(column.cards, column.cardOrder, "id");
 
+  //handle
+  const onCardDrop = (dropResult) => {
+    console.log(dropResult);
+  };
+
   return (
     <div className="column">
-      <header>{column.title}</header>
-      <ul className="card-list">
-        {cards.map((card, index) => (
-          <Card key={index} card={card} />
-        ))}
-      </ul>
+      <header className="column-drag-handle">{column.title}</header>
+      <div className="card-list">
+        <Container
+          groupName="patrickDev-columns"
+          onDrop={onCardDrop}
+          getChildPayload={(index) => cards[index]}
+          dragClass="card-ghost"
+          dropClass="card-ghost-drop"
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: "cards-drop-preview",
+          }}
+          dropPlaceholderAnimationDuration={200}
+        >
+          {cards.map((card, index) => (
+            <Draggable key={index}>
+              <Card card={card} />
+            </Draggable>
+          ))}
+        </Container>
+      </div>
       <footer>Add another </footer>
     </div>
   );
