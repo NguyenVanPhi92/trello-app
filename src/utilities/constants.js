c339a284cd1924a04213f48a4a3a56caa6810b6e
Phinv <nguyenvanphidmx123455@gmail.com>
2022-07-22 21:53:26 +0700

    deploy app

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,3 +1,4 @@
-export const API_ROOT = "http://localhost:8017";
-export const MODAL_ACTION_CLOSE = "MODAL_ACTION_CLOSE";
-export const MODAL_ACTION_CONFIRM = "MODAL_ACTION_CONFIRM";
+// export const API_ROOT = "http://localhost:8017";
+export const API_ROOT = 'https://trello-patrickdev-api.herokuapp.com';
+export const MODAL_ACTION_CLOSE = 'MODAL_ACTION_CLOSE';
+export const MODAL_ACTION_CONFIRM = 'MODAL_ACTION_CONFIRM';
